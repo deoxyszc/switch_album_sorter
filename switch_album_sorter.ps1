3f68b9c8441532f60d2fcabe951b385e8a949e9a
deoxyszc <45086445+deoxyszc@users.noreply.github.com>
2019-08-20 14:35:49 +0800

    上传初代脚本和游戏id对照表

---
+++ switch_album_sorter.ps1
@@ -0,0 +1,35 @@
+﻿$currentdir = (Split-Path -Parent $MyInvocation.MyCommand.Definition)
+$inputdir = $currentdir + "\Album"
+$outputdir = $currentdir + "\Album-sorted"
+$CONF = (Get-Content "game-id.json") | ConvertFrom-Json
+#Write-Host $currentdir
+
+if (-not (Test-Path -Path $outputdir))
+{
+    New-Item -Path $currentdir -Name "Album-sorted" -type "directory"
+}
+
+$fileList = Get-ChildItem  $inputdir -recurse | ?{$_.PsIsContainer -eq $false -and $_.Name -match "\w.jpg$|\w.mp4$"} | %{$_.FullName}
+Foreach ($file in $fileList)
+{
+    $filename_parse = (Get-Item $file).Basename -Split "-"
+    $gamename = $CONF.($filename_parse[1])
+    If($gamename -eq $null) 
+    {
+        $gamename = '其他'
+    }
+    $tempdir = $outputdir + '\' + $gamename
+    $newdir  = $tempdir + '\' + (Split-Path -Leaf $file)
+    if (-not (Test-Path -Path $tempdir))
+    {
+        New-Item -Path $outputdir -Name $gamename -type "directory"
+    }
+    #$filename = (Get-Item $file).Basename
+    #Write-Host $gamename
+    if (-not (Test-Path -Path $newdir))
+    {
+        Write-Host $file
+        Write-Host $newdir
+        Copy-Item $file $newdir
+    }
+}
